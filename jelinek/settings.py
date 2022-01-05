88f62f7866f6d95757eedfea9405813b7e44a2b4
steffres <stefan.resch@oeaw.ac.at>
2022-01-05 11:10:34 +0100

    Added ontology specific setting templates

---
+++ jelinek/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': None,
+        'USER': None,
+        'PASSWORD': None,
+        'HOST': None,
+        'PORT': None,
+    }
+}
