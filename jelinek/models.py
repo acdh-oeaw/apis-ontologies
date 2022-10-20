ddcb68e6e489514353c157224c38d0bd7b8115c3
Katharina Wünsche <katharina_wunsche@yahoo.de>
2022-10-20 16:33:29 +0200

    Add E40 to "is about" relation

--- jelinek/models.py
+++ jelinek/models.py
@@ -499,3 +499,4 @@ def construct_properties():
     is_about.subj_class.add(ContentType.objects.get(model=F1_Work.__name__))
     is_about.obj_class.add(ContentType.objects.get(model=F1_Work.__name__))
     is_about.obj_class.add(ContentType.objects.get(model=F10_Person.__name__))
+    is_about.obj_class.add(ContentType.objects.get(model=E40_Legal_Body.__name__))
