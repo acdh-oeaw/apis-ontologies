02753587a3c961880371895fa0aab799078a80f2
steffres <stefan.resch@oeaw.ac.at>
2023-04-05 17:14:46 +0200

    add serializer example

---
+++ base_project/additional_serializers.py
@@ -0,0 +1,50 @@
+from apis_ontology.models import *
+from apis_core.apis_relations.models import *
+from apis_core.apis_metainfo.models import *
+from apis_core.api_routers import AdditionalSerializerConfig
+
+
+additional_serializers_list = [
+    AdditionalSerializerConfig(
+        url="additional/example_serializer",
+        name="some_example_serializer",
+        path_structure={
+            F1_Work: [
+                F1_Work.id,
+                F1_Work.name,
+                F1_Work.idno,
+                {
+                    F1_Work.triple_set_from_subj: [
+                        {
+                            Triple.prop: [
+                                Property.name_forward,
+                                Property.name_reverse,
+                            ]
+                        },
+                        {
+                            Triple.obj: [
+                                F3_Manifestation_Product_Type.name,
+                                F3_Manifestation_Product_Type.bibl_id,
+                                {
+                                    F3_Manifestation_Product_Type.triple_set_from_obj: [
+                                        {
+                                            Triple.prop: [
+                                                Property.name_forward,
+                                                Property.name_reverse,
+                                            ]
+                                        },
+                                        {
+                                            Triple.subj: [
+                                                F10_Person.name
+                                            ],
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                    ]
+                },
+            ]
+        }
+    )
+]
