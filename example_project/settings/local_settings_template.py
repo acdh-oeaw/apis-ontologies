69571b4c477bb8498625aeeb61b16cfe1da8f86e
KK <dev-kk@oeaw.ac.at>
2023-04-17 22:07:29 +0200

    Clean up, add to local_settings_template
    - remove APIS-Bibsonomy example settings
    - add APIS_BASE_URL

--- example_project/settings/local_settings_template.py
+++ example_project/settings/local_settings_template.py
@@ -1,17 +1,17 @@
-# Template for file for local Django settings.
-#
-# To be used as basis for new settings files for local development,
-# to store keys/passwords, values for development databases etc.
-#
-# Resulting local settings files are not meant to be committed/versioned.
+"""
+Template for file for local Django settings.
 
+To be used as basis for new settings file(s) for local development,
+to store keys/passwords, configs for development databases etc.
+
+Resulting local settings files are not meant to be committed/versioned.
+"""
 from .server_settings import *
 
-# DEBUG, DEV_VERSION are optional variables which override defaults
-# for production environments set in server_settings.py
-# Comment them out if/when they are not needed.
-DEBUG = True
-DEV_VERSION = False
+# DEBUG, DEV_VERSION are optional variables which override defaults for
+# production environments which are set in server_settings.py
+# DEBUG = True
+# DEV_VERSION = False
 
 # Database settings for MySQL / MariaDB
 DATABASES = {
@@ -25,25 +25,5 @@ DATABASES = {
     }
 }
 
-
-# Bibsonomy example settings
-APIS_BIBSONOMY = [
-    {
-        "type": "zotero",
-        "url": "https://api.zotero.org/",
-        "user": "exmaple_user_id",  # fill in the API user ID, not the user-name
-        "API key": "example_key",
-    }
-]
-# Zotero example settings (note that code-wise the module and settings are
-# called bibsonomy eventhough they also support zotero)
-APIS_BIBSONOMY = [
-    {
-        "type": "bibsonomy",
-        "url": "https://www.bibsonomy.org/",
-        "user": "example_user",
-        "API key": "example_key",
-    }
-]
-# then append the bibsonomy module:
-INSTALLED_APPS.append("apis_bibsonomy")
+# For local development, override project-specific APIS_BASE_URI
+APIS_BASE_URI = ""
