2df036c4118ce98b5ee5bf206b5068aa3ff46a89
Richard Hadden <richard.hadden@oeaw.ac.at>
2023-04-20 12:40:41 +0200

    added models

--- manmax/custom_context_processors.py
+++ manmax/custom_context_processors.py
@@ -5,10 +5,11 @@ from apis_ontology.models import group_order
 
 
 def get_entity_groups():
-    entity_groups = defaultdict(list)
+    entity_groups = defaultdict(lambda: defaultdict(list))
     for cls in get_all_entity_classes():
         group = getattr(cls, "__entity_group__", "Other")
-        entity_groups[group].append((cls.__name__.lower(), cls._meta.verbose_name_plural.title()))
+        entity_type = getattr(cls, "__entity_type__", "Entity")
+        entity_groups[group][entity_type].append((cls.__name__.lower(), cls._meta.verbose_name_plural.title(), cls.__doc__))
     return entity_groups
 
 
@@ -20,3 +21,4 @@ def grouped_menus(request):
         if len(entity_groups[key]) > 0:
             groups[key] = entity_groups[key]
     return {"entity_groups": groups, "request": request}
+
--- manmax/models.py
+++ manmax/models.py
@@ -16,21 +16,23 @@ from apis_ontology.helper_functions import remove_extra_spaces
 
 # Entity categories
 
-BASE_ENTITIES = "Base Entities"
-BASE_STATEMENTS = "Base Statements"
-ART_ENTITIES = "Art Entities"
-ART_STATEMENTS = "Art Statements"
-MUSIC_ENTITIES = "Music Entities"
-MUSIC_STATEMENTS = "Music Statements"
+GENERIC = "Generic"
+ROLE_ORGANISATIONS = "Roles/Organisations"
+LIFE_FAMILY = "Life/Family"
+ART = "Art"
+MUSIC = "Music"
 OTHER = "Other"
 
+
+ENTITY = "Entities"
+STATEMENT = "Statements"
+
 group_order = [
-    BASE_ENTITIES,
-    BASE_STATEMENTS,
-    ART_ENTITIES,
-    ART_STATEMENTS,
-    MUSIC_ENTITIES,
-    MUSIC_STATEMENTS,
+    GENERIC,
+    LIFE_FAMILY,
+    ROLE_ORGANISATIONS,
+    ART,
+    MUSIC,
     OTHER,
 ]
 
@@ -43,7 +45,8 @@ class Person(TempEntityClass):
     class Meta:
         verbose_name = "Person"
 
-    __entity_group__ = BASE_ENTITIES
+    __entity_group__ = GENERIC
+    __entity_type__ = ENTITY
 
     def update_label_from_namings(self):
         """Get all Naming objects which are subjects of Triples where self is the object, and combine the values"""
@@ -67,23 +70,26 @@ class Person(TempEntityClass):
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Place(TempEntityClass):
-    """Place: a real place, identified by a label and (one or more) URIS."""
+    """Place: a real place, identified by a label and URIs."""
 
-    __entity_group__ = BASE_ENTITIES
+    __entity_group__ = GENERIC
+    __entity_type__ = ENTITY
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Organisation(TempEntityClass):
-    """Organisation: a real person, identified by a label and (one or more) URIS."""
+    """Organisation: an organisation or other group, identified by a label and URIs."""
 
-    __entity_group__ = BASE_ENTITIES
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = ENTITY
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class GenericWork(TempEntityClass):
     """A Work of art, literature, music, etc. Where possible, use specific subtypes (Artistic Work, Music Work, etc.)"""
 
-    __entity_group__ = BASE_ENTITIES
+    __entity_group__ = GENERIC
+    __entity_type__ = ENTITY
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
@@ -91,7 +97,16 @@ class GenericItem(TempEntityClass):
     """An item which is not (as far as this project is concerned) created by a Person — in which case, use Generic Work — but
     which may be owned or exchanged."""
 
-    __entity_group__ = BASE_ENTITIES
+    __entity_group__ = GENERIC
+    __entity_type__ = ENTITY
+
+
+@reversion.register(follow=["tempentityclass_ptr"])
+class Role(TempEntityClass):
+    """A Role in an Organisation, occupied by one or more Person in RoleOccupation, or assigned/removed via AssignmentToRole/RemovalFromRole"""
+
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = ENTITY
 
 
 # Base Work subtypes
@@ -101,14 +116,16 @@ class GenericItem(TempEntityClass):
 class MusicWork(GenericWork):
     """A piece of music"""
 
-    __entity_group__ = MUSIC_ENTITIES
+    __entity_group__ = MUSIC
+    __entity_type__ = ENTITY
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class ArtWork(GenericWork):
     """A work of art"""
 
-    __entity_group__ = ART_ENTITIES
+    __entity_group__ = ART
+    __entity_type__ = ENTITY
 
 
 # Base Statements
@@ -118,12 +135,14 @@ class ArtWork(GenericWork):
 class GenericStatement(TempEntityClass):
     """A Generic Statement about a Person (to be used when nothing else will work)."""
 
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = GENERIC
+    __entity_type__ = STATEMENT
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Naming(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = GENERIC
+    __entity_type__ = STATEMENT
 
     forename = models.CharField(
         max_length=200,
@@ -168,31 +187,81 @@ def update_person_label_on_naming_change(sender, instance: Naming, **kwargs):
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Birth(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = LIFE_FAMILY
+    __entity_type__ = STATEMENT
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Death(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = LIFE_FAMILY
+    __entity_type__ = STATEMENT
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Election(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = STATEMENT
+
     position = models.CharField(
         max_length=200, blank=True, verbose_name="Position elected to"
     )
 
+@reversion.register(follow="tempentityclass_ptr")
+class OrganisationLocation(GenericStatement):
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = STATEMENT
+
 
 @reversion.register(follow=["tempentityclass_ptr"])
-class Membership(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
-    role = models.CharField(blank=True, max_length=500)
+class Performance(GenericStatement):
+    __entity_group__ = GENERIC
+    __entity_type__ = STATEMENT
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
-class ParentalRelation(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+class RoleOccupation(GenericStatement):
+    """Describes the occupation of a Role by a Person"""
+
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = STATEMENT
+
+
+@reversion.register(follow=["tempentityclass_ptr"])
+class AssignmentToRole(GenericStatement):
+    """Describes the assignment of a Role to a Person (assignee), by an Person (assigner)"""
+
+    class Meta:
+        verbose_name_plural = "Assignments to Role Occupation"
+
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = STATEMENT
+
+    description = models.CharField(max_length=200)
+
+
+@reversion.register(follow=["tempentityclass_ptr"])
+class RemovalFromRole(GenericStatement):
+    """Describes the removal from a Role of a Person (role occupier), by an Person (remover)"""
+
+    class Meta:
+        verbose_name_plural = "Removals from Role Occupation"
+
+    __entity_group__ = ROLE_ORGANISATIONS
+    __entity_type__ = STATEMENT
+
+    description = models.CharField(max_length=200)
+
+
+@reversion.register(follow=["tempentityclass_ptr"])
+class FamilialRelation(GenericStatement):
+    __entity_group__ = LIFE_FAMILY
+    __entity_type__ = STATEMENT
+
+
+@reversion.register(follow=["tempentityclass_ptr"])
+class ParentalRelation(FamilialRelation):
+    __entity_group__ = LIFE_FAMILY
+    __entity_type__ = STATEMENT
     PARENTAL_TYPES = (
         ("mother", "Mother"),
         ("father", "Father"),
@@ -204,22 +273,33 @@ class ParentalRelation(GenericStatement):
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
+class SiblingRelation(FamilialRelation):
+    __entity_group__ = LIFE_FAMILY
+    __entity_type__ = STATEMENT
+
+
+@reversion.register(follow=["tempentityclass_ptr"])
 class Payment(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+    """Payment of money for Item, Work, Acquisition, or other Activity"""
+
+    __entity_group__ = GENERIC
+    __entity_type__ = STATEMENT
+
     amount = models.IntegerField(blank=True)
-    payment_for = models.CharField(blank=True, max_length=500)
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Order(GenericStatement):
     """An order given by someone to do something"""
 
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = GENERIC
+    __entity_type__ = STATEMENT
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Acquisition(GenericStatement):
-    __entity_group__ = BASE_STATEMENTS
+    __entity_group__ = GENERIC
+    __entity_type__ = STATEMENT
 
 
 # Art Statements
@@ -227,25 +307,29 @@ class Acquisition(GenericStatement):
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class ArtworkCreation(GenericStatement):
-    __entity_group__ = ART_STATEMENTS
+    __entity_group__ = ART
+    __entity_type__ = STATEMENT
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class ArtworkCommission(GenericStatement):
-    __entity_group__ = ART_STATEMENTS
+    __entity_group__ = ART
+    __entity_type__ = STATEMENT
 
 
+""" REMOVED: adds nothing above Acquisition
 @reversion.register(follow=["tempentityclass_ptr"])
-class ArtworkExchange(Acquisition):
+class ArtworkAcquisition(Acquisition):
     __entity_group__ = ART_STATEMENTS
-
+"""
 
 # Music Statements
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class MusicCreation(GenericStatement):
-    __entity_group__ = MUSIC_STATEMENTS
+    __entity_group__ = MUSIC
+    __entity_type__ = STATEMENT
 
     class Meta:
         verbose_name = "Music Creation"
@@ -253,16 +337,9 @@ class MusicCreation(GenericStatement):
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class MusicPerformance(GenericStatement):
-    __entity_group__ = MUSIC_STATEMENTS
+    __entity_group__ = MUSIC
+    __entity_type__ = STATEMENT
 
-
-class IdentificationOfEntity(AbstractReification):
-    identified_by = models.CharField(max_length=100, choices=(
-        ("explicit_in_source", "Explicit in Source"), 
-        ("implicit_in_source", "Implicit in Source"), 
-        ("inferred_from_other_source", "Inferred from Other Source"), 
-        ("guess", "Guess"),
-    ))
 
 def build_property(
     name: str,
@@ -287,36 +364,78 @@ def build_property(
 
 
 def construct_properties():
+    # Place located within
+    place_located_in_place = build_property("located within", "contains place", Place, Place)
 
+    # Naming
     naming_of_person = build_property("is naming of", "has naming", Naming, Person)
 
-
-
-    birth_event_to_identification = build_property("is birth of", "has birth", Birth, IdentificationOfEntity)
-    identification_to_person = build_property("has birth identification", "is identification of", IdentificationOfEntity, Person)
-
-
-
-
+    # Birth to person and location
+    birth_event_of_person = build_property("is birth of", "has birth", Birth, Person)
     birth_event_in_location = build_property(
         "has location", "is location of birth", Birth, Place
     )
 
+    # Death to person and location
     death_event_of_person = build_property("is death of", "has death", Death, Person)
     death_event_in_location = build_property(
         "has location", "is location of death", Death, Place
     )
 
-    membership_concerns_person = build_property(
-        "membership concerns person", "has membership", Membership, Person
-    )
-    membership_of_organisation = build_property(
-        "membership organisation",
-        "organisation of membership",
-        Membership,
-        Organisation,
+    # Organisation has location
+    organisation_location_place = build_property("organisation located in", "organisation location", OrganisationLocation, Place)
+    organisation_location_organisation = build_property("organisation with location", "organisation location", OrganisationLocation, Organisation)
+
+    # Role in organisation
+    role_in_organisation = build_property(
+        "role in organisation", "organisation has role", Role, Organisation
     )
 
+    # Role occupied by person with role occupation
+    role_occupied = build_property(
+        "role occupied",
+        "role occupation",
+        RoleOccupation,
+        Role,
+    )
+    role_occupation_occupied_by_person = build_property(
+        "person occupying role", "occupies role", RoleOccupation, Person
+    )
+    # Assignment of role
+    assignment_to_role_role = build_property(
+        "role assigned", "role assigned in", Role, AssignmentToRole
+    )
+    assignment_to_role_assigner = build_property(
+        "assigns role", "role assignment by", Person, AssignmentToRole
+    )
+    assignment_to_role_assignee = build_property(
+        "person assigned role", "role assigned to", Person, AssignmentToRole
+    )
+    assignment_to_role_leads_to_role_occupation = build_property(
+        "leads to role occupation",
+        "result of role assignment",
+        AssignmentToRole,
+        RoleOccupation,
+    )
+
+    # Removal from role
+    removal_from_role_role = build_property(
+        "role removed from", "removal of role via", Role, RemovalFromRole
+    )
+    removal_from_role_remover = build_property(
+        "removes from role", "role removal by", Person, RemovalFromRole
+    )
+    removal_from_role_person_removed = build_property(
+        "removed from role", "was removed by from role via", Person, RemovalFromRole
+    )
+    removal_from_role_ends_role_occupation = build_property(
+        "causes end of role occupation",
+        "ends as a result of removal",
+        RemovalFromRole,
+        RoleOccupation,
+    )
+
+    # Parental relation to person-parent and person-child
     parental_relation_parent = build_property(
         "has parent", "is parent in relation", ParentalRelation, Person
     )
@@ -326,8 +445,18 @@ def construct_properties():
 
     payment_payer = build_property("has payer", "is payer of", Payment, Person)
     payment_payee = build_property("has payee", "is payee of", Payment, Person)
-    payment_acquisition = build_property(
-        "for acquisition", "payment for", Payment, Acquisition
+    payment_for = build_property(
+        "payment for",
+        "result of payment",
+        Payment,
+        [
+            Acquisition,
+            ArtworkCommission,
+            ArtworkCreation,
+            ArtWork,
+            Performance,
+            MusicPerformance,
+        ],
     )
 
     election_of_person = build_property(
@@ -336,23 +465,42 @@ def construct_properties():
     location_of_election = build_property(
         "has location", "is location of election", Election, Place
     )
-    organisation_to_which_elected = build_property(
-        "organisation_elected_to", "organisation_of_election", Election, Organisation
+    role_to_which_elected = build_property(
+        "role elected to", "role filled via election", Election, Role
+    )
+    role_occupation_to_which_elected = build_property(
+        "role occupation resulting from", "resulted from", Election, RoleOccupation
     )
 
-    ordered_by_person = build_property("order given by", "gave order", Order, Person)
+    order_given_by_person = build_property(
+        "order given by", "gave order", Order, Person
+    )
     thing_ordered = build_property(
-        "thing ordered", "was result of order", Order, [Payment, Death, Acquisition]
+        "thing ordered",
+        "was result of order",
+        Order,
+        [
+            Payment,
+            Death,
+            Acquisition,
+            Order,
+            AssignmentToRole,
+            RemovalFromRole,
+            ArtworkCommission,
+            ArtworkCreation,
+            MusicCreation,
+            MusicPerformance,
+        ],
     )
 
-    ownership_by_person_start = build_property(
-        "start owner", "is start owner of", Acquisition, [Person, Organisation]
+    ownership_by_start = build_property(
+        "previous owner", "is previous owner of", Acquisition, [Person, Organisation]
     )
-    ownership_by_person_start = build_property(
-        "end owner", "is end owner of", Acquisition, [Person, Organisation]
+    ownership_by_end = build_property(
+        "new owner", "is new owner of", Acquisition, [Person, Organisation]
     )
     ownership_of_entity = build_property(
-        "thing owned", "is thing owned", Acquisition, [ArtWork, GenericItem]
+        "thing changing ownership", "changed ownership by", Acquisition, [ArtWork, GenericItem]
     )
 
     artwork_comissioned = build_property(
@@ -368,16 +516,22 @@ def construct_properties():
         Person,
     )
 
-    artwork_created = build_property(
+    artwork_created_in_artwork_creation = build_property(
         "artwork created", "is result of creation", ArtworkCreation, ArtWork
     )
-    artwork_created_by = build_property(
+    artwork_created_by_person = build_property(
         "created by", "person created artwork", ArtworkCreation, Person
     )
     artwork_creation_result_of_commission = build_property(
         "resulted from commission",
         "was result of commission",
         ArtworkCreation,
+        ArtworkCommission,
+    )
+    artwork_result_of_commission = build_property(
+        "resulted from commission",
+        "was result of commission",
+        ArtWork,
         ArtworkCommission,
     )
 
@@ -387,3 +541,5 @@ def construct_properties():
     music_created_by = build_property(
         "created by", "person created music work", MusicCreation, Person
     )
+
+    music_performance_of_music_work = build_property("music perfmored", "has performance", MusicPerformance, MusicWork)
