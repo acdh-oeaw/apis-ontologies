8a413c3a7ae1126a1a9b0f84e49d1fdfa972d876
Birger Schacht <birger@rantanplan.org>
2023-06-22 13:09:03 +0200

    sicprod: use sicprod branch of apis-core-rdf

--- sicprod/start.sh
+++ sicprod/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pip install --user git+https://github.com/acdh-oeaw/apis-core-rdf@e31351d597e90a9d3ca3a0400b06df758f766bb5
+pip install --user git+https://github.com/acdh-oeaw/apis-core-rdf@sicprod
