6619309a202586e2986eb53024c400ab343000f9
Gytha Ogg <gythaoggscat@gmail.com>
2023-07-06 04:06:46 +0200

    Update Person model

--- tibschol/models.py
+++ tibschol/models.py
@@ -1,6 +1,7 @@
 import reversion
 from apis_core.apis_entities.models import TempEntityClass
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
@@ -19,6 +20,14 @@ class Instance(TempEntityClass):
 class Person(TempEntityClass):
 
     class_uri = "http://id.loc.gov/ontologies/bibframe/Person"
+    GENDERS = [
+        ("male", "Male"),
+        ("female", "Female"),
+    ]
+
+    gender = models.CharField(max_length=6, choices=GENDERS, default="male")
+    comments = models.TextField(blank=True, null=True)
+    external_link = models.URLField(max_length=255, blank=True, null=True)
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
