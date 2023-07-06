b47a121469ee2997c6dfd224dfa53efd31ccca1f
Gytha Ogg <gythaoggscat@gmail.com>
2023-07-06 05:57:19 +0200

    Revert "Update Instance model"
    This reverts commit 3be456b13ad0d6ed17d4febc6426c1bdb0f67bc0.

--- tibschol/models.py
+++ tibschol/models.py
@@ -1,82 +1,19 @@
-import logging
-
 import reversion
 from apis_core.apis_entities.models import TempEntityClass
 from django.db import models
 from multiselectfield import MultiSelectField
-
-logger = logging.getLogger(__name__)
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
 class Instance(TempEntityClass):
 
     class_uri = "http://id.loc.gov/ontologies/bibframe/Instance"
-    SETS = [
-        ("Set 1", "Set 1"),
-        ("Set 2", "Set 2"),
-        ("Set 3", "Set 3"),
-        ("Set 4", "Set 4"),
-    ]
-    set_num = models.CharField(max_length=5,
-                               choices=SETS,
-                               null=True,
-                               verbose_name="Set")
-    volume = models.CharField(max_length=255, blank=True, null=True)
-    sb_text_number = models.CharField(
+    citation = models.CharField(
         max_length=255,
         blank=True,
         null=True,
-        verbose_name="Number ascribed to item by Tibschol",
+        verbose_name="citation",
     )
-    pp_kdsb = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        verbose_name="Page numbers in print",
-    )
-    num_folios = models.CharField(max_length=255,
-                                  blank=True,
-                                  null=True,
-                                  verbose_name="Number of folios")
-
-    signature_letter = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        verbose_name="Signature letter (category)",
-    )
-    signature_number = models.CharField(max_length=255,
-                                        blank=True,
-                                        null=True,
-                                        verbose_name="Signature number")
-    drepung_number = models.CharField(max_length=255,
-                                      blank=True,
-                                      null=True,
-                                      verbose_name="Drepung catalogue number")
-    provenance = models.CharField(max_length=255,
-                                  blank=True,
-                                  null=True,
-                                  verbose_name="Provenance")
-    comments = models.TextField(blank=True, null=True)
-    external_link = models.URLField(max_length=255, blank=True, null=True)
-
-    @property
-    def citation(self):
-        if self.set_num == '1':
-            return f"bKa' gdams gsung ‘bum phyogs bsgrigs theng dang po, vol. {self.volume}, dPal brtsegs bod yig dpe rnying zhib 'jug khang [dPe sgrig 'gan 'khur ba: dByang can lha mo et al.], Chengdu [khreng tu’u]: Si khron mi rigs dpe skrun khang, 2006, pp. {self.pp_kdsb}."
-
-        if self.set_num == '2':
-            return f"bKa' gdams gsung ‘bum phyogs bsgrigs theng gnyis pa, vol. {self.volume}, dPal brtsegs bod yig dpe rnying zhib 'jug khang [dPe sgrig 'gan 'khur ba: dByang can lha mo et al.], Chengdu [khreng tu’u]: Si khron mi rigs dpe skrun khang, 2007, pp. {self.pp_kdsb}."
-
-        if self.set_num == 3:
-            return f"bKa' gdams gsung ‘bum phyogs bsgrigs theng gsum pa, vol. {self.volume}, dPal brtsegs bod yig dpe rnying zhib 'jug khang [dPe sgrig 'gan 'khur ba: dByang can lha mo et al.], Chengdu [khreng tu’u]: Si khron mi rigs dpe skrun khang, 2009, pp. {self.pp_kdsb}."
-
-        if self.set_num == 4:
-            return f""
-
-        logger.warn(f"Unknown set: {self.set_num}. Cannot build citation.")
-        return f"Unknown set: {self.set_num}. Cannot build citation."
 
 
 @reversion.register(follow=["tempentityclass_ptr"])
