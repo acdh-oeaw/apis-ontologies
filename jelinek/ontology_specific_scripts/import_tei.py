03f6b16745d89d71ff238a5ac33a1ff70a95284f
Katharina Wünsche <katharina_wunsche@yahoo.de>
2023-08-18 13:37:46 +0200

    Bugfix in seklit import

--- jelinek/ontology_specific_scripts/import_tei.py
+++ jelinek/ontology_specific_scripts/import_tei.py
@@ -4220,13 +4220,16 @@ class TreesManager:
                                                     break
 
                                         if entity_chapter.chapter_number.startswith(("6", "7")):
+                                            for path_node_div_child in path_node_div.path_node_children_list:
 
-                                            for f1 in find_all_inner_f1_entities(path_node_div):
-                                                create_triple(
-                                                                entity_subj=f1,
-                                                                entity_obj=entity_chapter,
-                                                                prop=Property.objects.get(name="is in chapter"),
-                                                            )
+                                                if path_node_div_child.xml_elem.attrib.get("type", "") in ["seklitSubsection", "stagingSeklit"]:
+
+                                                    for f1 in find_all_inner_f1_entities(path_node_div_child):
+                                                        create_triple(
+                                                                        entity_subj=f1,
+                                                                        entity_obj=entity_chapter,
+                                                                        prop=Property.objects.get(name="is in chapter"),
+                                                                    )
 
                                         break
 
