d336618d4170ebea49ec73d090c36cc60b3c13a1
Gytha Ogg <gythaoggscat@gmail.com>
2023-09-28 06:23:02 +0200

    include id in the custom filter

--- tibschol/filters.py
+++ tibschol/filters.py
@@ -3,7 +3,8 @@ from django.db.models import Q
 
 def name_filter(queryset, name, value):
     return queryset.filter(
-        Q(name__icontains=value)
+        Q(id__icontains=value)
+        | Q(name__icontains=value)
         | Q(alternative_names__icontains=value)
         | Q(external_link__endswith=value)
     )
@@ -12,7 +13,8 @@ def name_filter(queryset, name, value):
 def instance_name_filter(queryset, name, value):
     print("Name: %s, Value: %s", name, value)
     return queryset.filter(
-        Q(name__icontains=value)
+        Q(id__icontains=value)
+        | Q(name__icontains=value)
         | Q(alternative_names__icontains=value)
         | Q(tibschol_ref__icontains=value)
         | Q(external_link__endswith=value)
@@ -21,7 +23,8 @@ def instance_name_filter(queryset, name, value):
 
 def work_name_filter(queryset, name, value):
     return queryset.filter(
-        Q(name__icontains=value)
+        Q(id__icontains=value)
+        | Q(name__icontains=value)
         | Q(alternative_names__icontains=value)
         | Q(sde_dge_ref__icontains=value)
         | Q(external_link__endswith=value)
